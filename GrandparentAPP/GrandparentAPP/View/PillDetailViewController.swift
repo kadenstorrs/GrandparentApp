1db5173ea0c959b714b3c8b8d649901fb0f5f14b
Ella Neumarker <eneumarker@gmail.com>
2020-01-24 09:24:48 -0700

    Added all view controllers needed in storyboard

---
+++ GrandparentAPP/GrandparentAPP/View/PillDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PillDetailViewController.swift
+//  GrandparentAPP
+//
+//  Created by Ella  Neumarker on 1/23/20.
+//  Copyright © 2020 Kaden Storrs. All rights reserved.
+//
+
+import UIKit
+
+class PillDetailViewController: UIViewController {
+
+    @IBOutlet weak var circularImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        
+        circularImage.layer.masksToBounds = true
+        circularImage.layer.cornerRadius = circularImage.bounds.width / 2
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
